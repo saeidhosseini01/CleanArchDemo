b9f7ae5418f9f956d5fa2f9e375f552b5f0fcbcb
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-05-10 00:19:41 +0430

    userDtos

---
+++ CleanArchDemo.Applications/Dtos/UserDtos/Users/UserDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CleanArchDemo.Applications.Dtos.UserDtos.Users
+{
+    public class UserDto
+    {
+        [JsonPropertyName("id")]
+        public Double Id { get; set; }
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("Age")]
+        public double Age { get; set; }
+
+        [JsonPropertyName("Email")]
+        public string Email { get; set; }
+    }
+}
