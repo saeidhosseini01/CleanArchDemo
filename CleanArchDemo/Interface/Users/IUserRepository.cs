6b503fac6b958227b4c0f0625615b409fd5a0341
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-05-10 00:16:48 +0430

    CreateUserRepository

---
+++ CleanArchDemo/Interface/Users/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchDemo.Domain.Entitis.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchDemo.Domain.Interface.Users
+{
+    public interface IUserRepository
+    {
+
+        Task<List<User>> GetAllUsersAsync(CancellationToken cancellationToken);
+        Task<User> GetUserByIdAsync(int id,CancellationToken cancellationToken);
+        Task AddUserAsync(User user, CancellationToken cancellationToken);
+
+        Task UpdateUserAsync(User user, CancellationToken cancellationToken);
+
+    }
+}
