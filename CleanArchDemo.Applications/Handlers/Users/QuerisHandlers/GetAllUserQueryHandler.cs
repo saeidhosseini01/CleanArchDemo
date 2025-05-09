c825778e83a009afaece31cb7f3777aff87756ca
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-05-10 01:08:43 +0430

    CommandQuesryHandler

--- CleanArchDemo.Applications/Handlers/Users/QuerisHandlers/GetAllUserQueryHandler.cs
+++ CleanArchDemo.Applications/Handlers/Users/QuerisHandlers/GetAllUserQueryHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using CleanArchDemo.Applications.Dtos.UserDtos.Users;
+using CleanArchDemo.Applications.Queris.Users;
+using CleanArchDemo.Domain.Interface.Users;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,23 @@ using System.Threading.Tasks;
 
 namespace CleanArchDemo.Applications.Handlers.Users.QuerisHandlers
 {
-    internal class GetAllUserQueryHandler
+    public class GetAllUserQueryHandler 
+        : IRequestHandler<GetAllUserQuery, List<UserDto>>
     {
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+
+        public GetAllUserQueryHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<List<UserDto>> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
+        {
+            var users =await userRepository.GetAllUsersAsync(cancellationToken);
+            return mapper.Map<List<UserDto>>(users);
+           
+        }
     }
 }
