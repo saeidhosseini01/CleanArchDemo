306358c0b595492730926cef73d5d79275d2d11d
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-05-11 00:27:20 +0430

    dbcontext

--- CleanArchDemo.Persistance/DemoDbContexts/DemoDbContext.cs
+++ CleanArchDemo.Persistance/DemoDbContexts/DemoDbContext.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CleanArchDemo.Domain.Entitis.Users;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchDemo.Persistance.DemoDbContexts
 {
-    public class DemoDbContext:DbContext
+    public class DemoDbContext(DbContextOptions<DemoDbContext> options) :DbContext(options) 
     {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(user =>
+            {
+                user.Property(e => e.Id);
+                user.HasKey(e => e.Id);
+            });
+        }
+        public DbSet<User> Users { get; set; }
     }
 }
